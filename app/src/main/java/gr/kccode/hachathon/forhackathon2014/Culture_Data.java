cb476a44d8fca54835e15a7e917abb5fb6656e00
papazock <papazock@windowslive.com>
2014-11-29 18:28:08 -0800

    Database

---
+++ app/src/main/java/gr/kccode/hachathon/forhackathon2014/Culture_Data.java
@@ -0,0 +1,42 @@
+package gr.kccode.hachathon.forhackathon2014;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by Chris on 11/29/2014.
+ */
+public class Culture_Data extends BasicData {
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public static enum Type{CINEMA, THEATER, MUSEUM};
+    String label;
+    private String description;
+    private Type type;
+
+
+    public Culture_Data(){
+        super();
+    }
+
+    public Culture_Data(int ID,LatLng point, String label, String description,Type type){
+        super(ID, point, label);
+        this.setDescription(description);
+        this.setType(type);
+
+    }
+}
--- app/src/main/java/gr/kccode/hachathon/forhackathon2014/DbAdapter.java
+++ app/src/main/java/gr/kccode/hachathon/forhackathon2014/DbAdapter.java
@@ -3,5 +3,142 @@ package gr.kccode.hachathon.forhackathon2014;
 /**
  * Created by Chris on 11/29/2014.
  */
-public class DbAdapter {
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+
+final class DbAdapter {
+    private static DbAdapter Instance = new DbAdapter();
+
+    private DbAdapter() {
+    }
+
+    /**
+     * @return instance of this class
+     */
+    public static DbAdapter getInstance() {
+        return Instance;
+    }
+
+    /**
+     * array of hotspots
+     */
+    private ArrayList<Wifi_Data> hotspots = new ArrayList<Wifi_Data>();
+
+    /**
+     * array of Parking_Datas
+     */
+    private ArrayList<Parking_Data> Parking_Datas = new ArrayList<Parking_Data>();
+
+
+    /**
+     * array of Gashotspots
+     */
+    private ArrayList<GasStation_Data> Gashotspots = new ArrayList<GasStation_Data>();
+
+    /**
+     * array of dates
+     */
+    private ArrayList<Calendar_Data> dates = new ArrayList<Calendar_Data>();
+
+    /**
+     * array of timeline hotspots
+     */
+    private ArrayList<Culture_Data> cultura = new ArrayList<Culture_Data>();
+
+
+    /**
+     * dbHelper object to interact with the db
+     */
+    private DbHelper db;
+
+    /**
+     * The active Calendar_Data after a successful login
+     */
+    private Calendar_Data activeCalendar_Data;
+
+
+    /**
+     * Fills the arrays with data from the DB
+     */
+    private void fillArrays() {
+        hotspots = db.getAllhotspots();
+        Parking_Datas = db.getAllParking_Datas();
+        Gashotspots = db.getAllGashotspots();
+        dates = db.getAlldates();
+        cultura = db.getAllcultura();
+    }
+
+    /**
+     * @return all the hotspots in the DB
+     */
+    public ArrayList<Wifi_Data> gethotspots() {
+        return this.hotspots;
+    }
+
+    /**
+     * @return all the Parking_Datas in the DB
+     */
+    public ArrayList<Parking_Data> getParking_Datas() {
+        return Parking_Datas;
+    }
+
+
+    /**
+     * @return all the dates in the DB
+     */
+    public ArrayList<Calendar_Data> getdates() {
+        return dates;
+    }
+
+    /**
+     * @return all the Gashotspots in the DB
+     */
+    public ArrayList<GasStation_Data> getGashotspots() {
+        return Gashotspots;
+    }
+
+    /**
+     * @return all the timeline hotspots
+     */
+    public ArrayList<Culture_Data> getcultura() {
+        return this.cultura;
+    }
+
+    /**
+     * fills the db if data if the tables are empty and then fill the arrays with the data from the db
+     *
+     * @param db
+     */
+    public void setDB(DbHelper db) {
+        this.db = db;
+        if (!hotspots.isEmpty()) {
+            this.hotspots.clear();
+            this.Parking_Datas.clear();
+            this.cultura.clear();
+            this.Gashotspots.clear();
+
+        }
+        this.fillArrays();
+
+    }
+
+    /**
+     *
+     * @param Parking_Data_id
+     * @return the hotspots that belong to the Parking_Data with this id
+     */
+
+
+    /**
+     * Inserts the data into the tables
+     */
+    private void populateDB() {
+
+    }
+
+
 }
